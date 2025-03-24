e07c97804be0aa2b85c9ef074fba0a93a1030e89
Darpan Kaushik <106373687+darpankaushik2002@users.noreply.github.com>
2025-03-24 22:54:13 +0530

    Add files via upload

---
+++ python_programs/Day1/swap_two_var.py
@@ -0,0 +1,29 @@
+#AIM:- to swap two numbers
+"""
+#using third variable
+x=20
+y=10
+print("the initial value of x and y are",x,y)
+temp=x
+print("the value of temp var is",20)
+x=y
+print("the value of x is",x)
+y=temp
+print("the value of y is ",y)
+"""
+'''
+# without using third variable
+x=35
+y=12
+print("the initial value of x and y is",x,y)
+x,y=y,x
+print("the final value of x and y is",x,y)
+'''
+"""
+#taking input from user
+x=int(input("Enter any int number:-"))
+y=int(input("Enter any int number:-"))
+print("the initial value of x and y is",x,y)
+x,y=y,x
+print("the final value of x and y is",x,y)
+"""
