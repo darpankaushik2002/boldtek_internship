433a9c4e67a6a8ee54dd5516d0c843c2230ecc60
Darpan Kaushik <106373687+darpankaushik2002@users.noreply.github.com>
2025-04-09 23:24:14 +0530

    Add files via upload

---
+++ SmartDoc AI (mid_term_project)/pdf_utils.py
@@ -0,0 +1,14 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+def load_and_split_pdf(pdf_path):
+    loader = PyPDFLoader(pdf_path)
+    pages = loader.load_and_split()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+
+    split_docs = text_splitter.split_documents(pages)
+    return split_docs
