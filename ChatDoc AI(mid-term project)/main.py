59838c52949317c77c5f42e71c39aa5c537c6e73
Darpan Kaushik <106373687+darpankaushik2002@users.noreply.github.com>
2025-04-19 11:54:31 +0530

    Delete ChatDoc AI(mid-term project) directory

--- ChatDoc AI(mid-term project)/main.py
+++
@@ -1,7 +0,0 @@
-from pdf_utils import load_and_split_pdf
-from vectordb_utils import save_to_faiss
-
-pdf_path = "data/ml_book.pdf"
-docs = load_and_split_pdf(pdf_path)
-save_to_faiss(docs)
-print("✅ PDF processed and stored in FAISS vector store.")
